8ec7bb1172d6d5b9127d5c2869789360708bd43a
omar-ro0724 <omars2480@gmail.com>
2025-02-24 20:20:31 -0500

    switch nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("OMAR RODRIGUEZ :)");
+public class Main{
+        public static void main(String[] args) {
+            int dia = 3;
 
+            String nombreDia = switch (dia) {
+                case 1 -> "Lunes";
+                case 2 -> "Martes";
+                case 3 -> "Miércoles";
+                case 4 -> "Jueves";
+                case 5 -> "Viernes";
+                case 6 -> "Sábado";
+                case 7 -> "Domingo";
+                default -> "Día inválido";
+            };
+
+            System.out.println("Día seleccionado: " + nombreDia);
+        }
     }
-}
+
+
